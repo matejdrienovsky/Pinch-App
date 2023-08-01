c893adf891bd3d0aac7e4aea2c90f284d90d7274
Matej Drienovský <matejdrienovsky123@gmail.com>
2023-08-01 13:02:47 +0200

    first commit

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Ivo Pecivko on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
